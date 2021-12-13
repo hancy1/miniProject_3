5693aa11c5f3cfe5eeb3ff20683485204d9bc64d
halas8 <pioneercmh@gmail.com>
2021-12-13 16:16:29 +0900

    다시 만들었어요

---
+++ src/com/uni/first/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.uni.first;
+
+public class HelloWorld {
+
+		public void method1() {
+			
+			System.out.println("Hello World!");
+			System.out.println("Hello World!");
+		}
+	
+}
---
+++ src/com/uni/run/RunA.java
@@ -0,0 +1,12 @@
+package com.uni.run;
+
+import com.uni.first.HelloWorld;
+
+public class RunA {
+
+	public static void main(String[] args) {
+		HelloWorld a = new HelloWorld();
+		a.method1();
+	}
+
+}
