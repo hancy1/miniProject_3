d0de1abf4afa1b7b18de720ae16fd9deb3bf7d0c
hancy1 <96040351+hancy1@users.noreply.github.com>
2021-12-13 16:39:39 +0900

    Update HelloWorld.java

--- src/com/uni/first/HelloWorld.java
+++ src/com/uni/first/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		public void method1() {
 			
 			System.out.println("Hello World!");
-			System.out.println("Hello World!");
+			System.out.println("Hello World! 테스트중");
 		}
 	
 }
