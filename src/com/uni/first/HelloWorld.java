d46a044382591cca76f0f9f9742dff44532ebc03
codud <codud@192.168.168.110>
2021-12-14 15:35:09 +0900

    채영 확인

--- src/com/uni/first/HelloWorld.java
+++ src/com/uni/first/HelloWorld.java
@@ -6,12 +6,11 @@ public class HelloWorld {
 			
 			System.out.println("Hello World!");
 			System.out.println("Hello World! 테스트중");
-<<<<<<< HEAD
 			System.out.println("Hello World!");
 			System.out.println("Hello World 채영");
-=======
+
 			System.out.println("Hello World !");
->>>>>>> branch 'master' of https://github.com/hancy1/miniProject_3
+
 		}
 	
 }
