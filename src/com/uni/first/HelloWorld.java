af2ca0eb126278b370b2400b7ffae38494f082f4
Lee Sanghwa <Lee Sanghwa@DESKTOP-BCFI12S>
2021-12-14 15:22:03 +0900

    HelloWorld 소스 수정

--- src/com/uni/first/HelloWorld.java
+++ src/com/uni/first/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		public void method1() {
 			
 			System.out.println("Hello World!");
-			System.out.println("Hello World! 테스트중");
+			System.out.println("소스 수정 중");
 		
 		}
 	
