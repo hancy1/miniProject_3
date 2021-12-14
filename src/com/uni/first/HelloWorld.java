89061267841e20575a2210c26fe02b0d6241d7e8
YoonJong Choi <sunk22@naver.com>
2021-12-14 16:43:56 +0900

    hello world! (lower case)

--- src/com/uni/first/HelloWorld.java
+++ src/com/uni/first/HelloWorld.java
@@ -6,7 +6,10 @@ public class HelloWorld {
 			
 			System.out.println("Hello World!");
 			System.out.println("Hello World! 테스트중");
-			System.out.println("Hello World!");
+			System.out.println("Hello World!!!");
+			System.out.println("hello world~~!!");
+			System.out.println("hello world~~!! test");
+			System.out.println("hello world~~!! test1");
 		}
 	
 }
