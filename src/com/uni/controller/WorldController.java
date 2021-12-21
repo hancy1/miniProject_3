747fa84488d674c9f36d586e3d72efde75f09f9d
imcha <imcha@LAPTOP-G9MDQ88M>
2021-12-21 11:54:05 +0900

    //임찬희_메뉴 번호 입력 시 예외 발생 try-catch

--- src/com/uni/controller/WorldController.java
+++ src/com/uni/controller/WorldController.java
@@ -3,16 +3,16 @@ package com.uni.controller;
 public class WorldController {
 
 	//전달 받은 관리자 아이디, 패스워드 일치하는지 확인
-	public int login(String id, String pwd) {
-		
-		//둘 중 하나라도 일치하지 않으면 0 리턴
-		if(!id.equals("kh3") || !pwd.equals("3333")) {
-			
-			return 0;
+	public boolean login(String id, String pwd) {
+
+		//둘 다 일치하면 true 리턴
+		if(id.equals("kh3") && pwd.equals("3333")) {
+
+			return true;
 		}
-		
-		//둘 다 일치하면 1 리턴
-		return 1;
+
+		//둘 중 하나라도 일치하지 않으면 false 리턴
+		return false;
 	}
 
 	
